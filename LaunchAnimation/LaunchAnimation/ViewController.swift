cfd6ea204b4e5866a4dd33a94d4017b0694d9b91
shawnDu <shuaishuai.du@smartereye.com>
2015-10-15 19:24:51 +0800

    create launch animation

---
+++ LaunchAnimation/LaunchAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchAnimation
+//
+//  Created by ShawnDu on 15/10/13.
+//  Copyright © 2015年 Beijing Smarter Eye Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
